f284ad2eaac0512738b176d9085ec555987f4f61
ialesanco <ikeralesanco2003gmail.com>
2022-03-03 11:49:53 +0100

    implementacion test testEliminarAlumno

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -0,0 +1,63 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+	public Curso c;
+
+	@BeforeEach
+	void beforeEach() {
+		c = new Curso();
+		c.aniadirAlumno(new Persona("123456789", "asdfsad", "asdf"));
+		c.aniadirAlumno(new Persona("25", "asdfsad", "asdf"));
+		c.aniadirAlumno(new Persona("123477789", "asdfsad", "asdf"));
+	}
+
+	@Test
+	void testEliminarAlumno() {
+		try {
+			int numBefore = c.numeroAlumnos();
+			c.eliminarAlumno("123456789");
+			int numAfter = c.numeroAlumnos();
+			assertEquals(numBefore - 1, numAfter);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	void testEliminarAlumnoError() {
+		assertThrows(Exception.class, () -> c.eliminarAlumno("1"));
+
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarTodos() {
+		fail("Not yet implemented");
+	}
+
+}
